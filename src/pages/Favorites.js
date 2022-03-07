57c2f8dc7a7d487f33356fcb5caa8087e0e70b36
Andre Felipe <afelipes@gmail.com>
2022-03-07 15:33:02 -0300

    1. Crie as rotas necessárias para a aplicação

---
+++ src/pages/Favorites.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites"></div>
+    );
+  }
+}
+
+export default Favorites;
