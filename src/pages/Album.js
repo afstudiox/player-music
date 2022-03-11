e4d63946fb2d9244259a5ef2408868f08ebbd64d
Andre Felipe <afelipes@gmail.com>
2022-03-11 02:01:18 -0300

    7. Em desenvolvimento :boa noite

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bandName: '',
+      albumName: '',
+      musicsAlbum: [],
+    };
+  }
+
+  componentDidMount() {
+    this.request();
+  }
+
+  // função assincrona para chamar a API
+  request = async () => {
+    const { musicsAlbum } = this.props.match.params;
+    const { id } = musicsAlbum;
+    const result = await getMusics(id);
+    console.log(result);
+  }
+
   render() {
     return (
       <div data-testid="page-album">
         <Header />
+        <div className="album-container">Em desenvolvimento</div>
       </div>
     );
   }
 }
 
+Album.propTypes = {
+  musicsAlbum: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+};
 export default Album;
