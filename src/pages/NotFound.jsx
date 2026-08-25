3dc468049533f4b1fd38b465efc3e4d6d9d752a7
Andre Felipe <afelipes@gmail.com>
2026-08-25 15:20:38 -0300

    refactor: migra NotFound para componente de função
    Componente sem estado e sem props: tradução direta, comportamento idêntico.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
-
-class NotFound extends Component {
-  render() {
-    return (
-      <div data-testid="page-not-found" />
-    );
-  }
+function NotFound() {
+  return (
+    <div data-testid="page-not-found" />
+  );
 }
 
 export default NotFound;
