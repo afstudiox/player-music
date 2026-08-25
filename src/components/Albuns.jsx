c269f65ad21413b8dfb624e15c9140bf6363b3fc
Andre Felipe <afelipes@gmail.com>
2026-08-25 16:16:11 -0300

    refactor: migra Albuns para componente de função
    Props destrinchadas nos parâmetros; remove prop 'search' inexistente no Router v5 (que vazava como atributo no <a>) e comentários órfãos.

--- src/components/Albuns.jsx
+++ src/components/Albuns.jsx
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-class Albuns extends Component {
-  render() {
-    const { result } = this.props;
-    const { artworkUrl100, collectionName, artistName, collectionId } = result;
-    return (
-      <Link
-        to={ `/album/${collectionId}` }
-        search={ collectionId }
-      >
-        <div className="album-item col" data-testid={ `link-to-album-${collectionId}` }>
-          <img className="album-image" src={ artworkUrl100 } alt="" />
-          <p className="album-artist">{ artistName }</p>
-          <p className="album-name">{ collectionName }</p>
-        </div>
-      </Link>
-    );
-  }
+function Albuns({ result }) {
+  const { artworkUrl100, collectionName, artistName, collectionId } = result;
+  return (
+    <Link to={`/album/${collectionId}`}>
+      <div className="album-item col" data-testid={`link-to-album-${collectionId}`}>
+        <img className="album-image" src={artworkUrl100} alt="" />
+        <p className="album-artist">{artistName}</p>
+        <p className="album-name">{collectionName}</p>
+      </div>
+    </Link>
+  );
 }
 
 Albuns.propTypes = {
@@ -31,7 +24,3 @@ Albuns.propTypes = {
 };
 
 export default Albuns;
-
-// collectionName
-// artistName
-// artworkUrl100
