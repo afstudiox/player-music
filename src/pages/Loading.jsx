1e6da201de088042e196b7e4de27801e1378cd91
Andre Felipe <afelipes@gmail.com>
2026-08-25 13:17:33 -0300

    refactor: migra Loading para componente de função

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
 import '../css/loading.css';
 
-class Loading extends Component {
-  render() {
-    return (
-      <div className="loading-container">
-        <div className="loading" />
-      </div>
-    );
-  }
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading" />
+    </div>
+  );
 }
 
 export default Loading;
